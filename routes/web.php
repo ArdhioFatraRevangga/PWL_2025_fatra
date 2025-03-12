0c330301470ee97051df800617d308c19ffd2e4d
ArdhioFatraRevangga <fatraoppo25@gmail.com>
2025-03-12 21:48:56 +0700

    Menambahkan View dengan Blade dan Controller

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-use App\Http\Controllers\PageController;
+use App\Http\Controllers\WelcomeController;
 
-Route::get('/', [PageController::class, 'index']);
-Route::get('/about', [PageController::class, 'about']);
-Route::get('/articles/{id}', [PageController::class, 'articles']);
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
+
+
