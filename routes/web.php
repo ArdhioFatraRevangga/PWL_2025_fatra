22b37d53c33c2fea18f97ad07d6d5c8a8761dcd2
ArdhioFatraRevangga <fatraoppo25@gmail.com>
2025-03-12 20:45:31 +0700

    routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::prefix('admin')->group(function () {
+    Route::get('/user', function () { return 'Admin User'; });
+    Route::get('/post', function () { return 'Admin Post'; });
 });
+
