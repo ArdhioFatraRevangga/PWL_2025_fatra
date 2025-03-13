8d0e9713cf9f722284f8fe0c99de04e659520932
ArdhioFatraRevangga <fatraoppo25@gmail.com>
2025-03-13 12:12:15 +0700

    Praktikum 6

---
+++ POS/resources/views/profile.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profil User</title>
+</head>
+<body>
+    <h2>Profil Pengguna</h2>
+    <p>ID: {{ $id }}</p>
+    <p>Nama: {{ $name }}</p>
+</body>
+</html>
---
+++ POS/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|+
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\UserController; // Tambahkan ini
+
+// Routing untuk Query Builder (Praktikum 5)
+Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
+Route::get('/category', [CategoryController::class, 'index']);
+Route::get('/category/add', [CategoryController::class, 'add']);
+Route::get('/category/update', [CategoryController::class, 'update']);
+Route::get('/category/delete', [CategoryController::class, 'delete']);
