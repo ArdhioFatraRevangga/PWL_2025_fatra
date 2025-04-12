fb7c535f504db4cf02132d1c9a671a39fdf17242
ArdhioFatraRevangga <fatraoppo25@gmail.com>
2025-04-12 20:25:30 +0700

    JS4-praktikum 2.4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,8 +10,17 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::where('level_id', 2)->count();
-        return view('user', ['data' => $user]);
+        $user = UserModel::firstOrNew(['username' => 'manager33']);
+
+    // Hanya set data jika belum ada di DB
+    if (!$user->exists) {
+        $user->nama = 'Manager Tiga Tiga';
+        $user->password = Hash::make('12345');
+        $user->level_id = 2;
+        $user->save();
     }
+
+    return view('user', ['data' => $user]);
+}
     
 }
