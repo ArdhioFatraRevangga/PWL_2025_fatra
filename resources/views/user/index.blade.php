31fcf8edfd8d022990663857dbb24e897006c776
ArdhioFatraRevangga <fatraoppo25@gmail.com>
2025-04-16 23:51:24 +0700

    JS5-praktikum 3

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,31 +1,70 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Data User</title>
-</head>
-<body>
-    <h1>Data User</h1>
-    <a href="{{ route('user.tambah') }}">Tambah User</a>
-    <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Nama</th>
-            <th>ID Level Pengguna</th>
-            <th>Aksi</th>
-        </tr>
-        @foreach ($data as $d)
-        <tr>
-            <td>{{ $d->user_id }}</td>
-            <td>{{ $d->username }}</td>
-            <td>{{ $d->nama }}</td>
-            <td>{{ $d->level_id }}</td>
-            <td>
-                <a href="{{ route('user.ubah', $d->user_id) }}">Ubah</a> |
-                <a href="{{ route('user.hapus', $d->user_id) }}">Hapus</a>
-            </td>
-        </tr>
-        @endforeach
-    </table>
-</body>
-</html>
+@extends('layouts.template')
+
+@section('content')
+    <div class="card card-outline card-primary">
+        <div class="card-header">
+            <h3 class="card-title">{{ $page->title }}</h3>
+            <div class="card-tools">
+                <a class="btn btn-sm btn-primary mt-1" href="{{ url('user/create') }}">Tambah</a>
+            </div>
+        </div>
+        <div class="card-body">
+            @if (session('succes'))
+            <div class="alert alert-success">{{ session('success') }}</div>
+            @endif
+            @if (session('error'))
+            <div class="alert alert-danger">{{ session('error') }}</div>
+            @endif
+            <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Username</th>
+                        <th>Name</th>
+                        <th>User
+                            Level</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+@endsection
+
+@push('css')
+@endpush
+
+@push('js')
+    <script>
+        $(document).ready(function () {
+            var dataUser = $('#table_user').DataTable({
+                serverSide: true,
+                serverSide: true, ajax: {
+                    "url": "{{ url('user/list') }}",
+                    "dataType": "json",
+                    "type": "POST"
+                },
+                columns: [
+                    {
+                        data: "DT_RowIndex",
+                        className: "text-center", orderable: false, searchable: false
+                    }, {
+                        data: "username", className: "",
+                        orderable: true, orderable: true,
+                        searchable: true, searchable: true
+                    }, {
+                        data: "nama", className: "",
+                        orderable: true, searchable: true
+                    }, {
+                        data: "level.level_nama",
+                        className: "",
+                        orderable: false, searchable: false
+                    }, {
+                        data: "action", className: "",
+                        orderable: false, searchable: false
+                    }
+                ]
+            });
+        }); 
+    </script>
+@endpush
--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,38 @@ use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\UserController; // Tambahkan ini
 
 // Routing untuk Query Builder (Praktikum 5)
-Route::get("/", [WelcomeController::class,"index"]);
-Route::get('/level', [LevelController::class,'index']);
-Route::get('/user/with-level', [UserController::class, 'index_with_level']);
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class,'index']);
-Route::get('/user/test', [UserController::class, 'index']);
-Route::get('/user/tambah', [UserController::class,'tambah'])->name('user.tambah');
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah'])->name('user.ubah');
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan'])->name('user.ubah_simpan');
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus'])->name('user.hapus');
-Route::get('/user', [UserController::class, 'index'])->name('user.index');
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan'])->name('user.tambah_simpan');
-Route::get('/kategori/add', [KategoriController::class, 'add']);
-Route::get('/kategori/update', [KategoriController::class, 'update']);
-Route::get('/kategori/delete', [KategoriController::class, 'delete']);
+// Route::get("/", [WelcomeController::class,"index"]);
+
+// Route::get('/level', [LevelController::class,'index']);
+// Route::get('/user/with-level', [UserController::class, 'index_with_level']);
+// Route::get('/kategori', [KategoriController::class, 'index']);
+// Route::get('/user', [UserController::class,'index']);
+// Route::get('/user/test', [UserController::class, 'index']);
+// Route::get('/user/tambah', [UserController::class,'tambah'])->name('user.tambah');
+// Route::get('/user/ubah/{id}', [UserController::class, 'ubah'])->name('user.ubah');
+// Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan'])->name('user.ubah_simpan');
+// Route::get('/user/hapus/{id}', [UserController::class, 'hapus'])->name('user.hapus');
+// Route::get('/user', [UserController::class, 'index'])->name('user.index');
+// Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan'])->name('user.tambah_simpan');
+// Route::get('/kategori/add', [KategoriController::class, 'add']);
+// Route::get('/kategori/update', [KategoriController::class, 'update']);
+// Route::get('/kategori/delete', [KategoriController::class, 'delete']);
+
+// Route:: get ('/', [WelcomeController :: class,'index' ]);
+
+Route::group(['prefix'=>'user'], function(){
+    Route::get('/', [UserController::class, 'index']);
+    Route::post('/list', [UserController::class, 'list']);
+    Route::get('/create', [UserController::class, 'create']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::get ('/create_ajax', [UserController:: class, 'create_ajax' ]);// Menampilkan halaman form tambah user Ajax
+    Route::post('/ajax', [UserController:: class, 'store_ajax' ]);    // Menyimpan data user baru Ajax
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::get('/{id}/edit', [UserController::class, 'edit']);
+    Route::put('/{id}', [UserController::class, 'update']);
+    Route::get('/{id}/edit_ajax', [UserController::class,'edit_ajax']); //Menampilkan Halaman form Edit User AJAX
+    Route::put('/{id}/update_ajax', [UserController::class,'update_ajax']); //Menyimpan perubahan data User AJAX
+    Route::get('/{id}/delete_ajax',[UserController::class,'confirm_ajax']);
+    Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']);
+    Route::delete('/{id}', [UserController::class, 'destroy']);
+});
