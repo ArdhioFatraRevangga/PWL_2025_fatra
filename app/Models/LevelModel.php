1eacf21c41b5cd7753d0f232b43ac3cad4b189dd
ArdhioFatraRevangga <fatraoppo25@gmail.com>
2025-04-17 05:00:21 +0700

    JS6-assignment

--- app/Models/LevelModel.php
+++ app/Models/LevelModel.php
@@ -8,12 +8,14 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class LevelModel extends Model
 {
-use HasFactory;
-protected $table = "m_level";
-protected $primaryKey = "level_id";
+    use HasFactory;
+    protected $table = "m_level";
+    protected $primaryKey = "level_id";
 
-public function users(): HasMany
-{
-return $this->hasMany(User :: class);
-}
+   //untuk mengizinkan mass assignment
+    protected $fillable = ['level_kode', 'level_nama'];
+
+    function  users() : HasMany {
+        return $this->hasMany(User::class);
+    }
 }
